349fb7d21567807a63df9996ba75fc9c299721bc
Tran Minh Sang <minhsang.1mil@gmail.com>
2024-09-11 00:00:38 +0700

    create resource file

--- src/Contracts/Common/Messages/Message.cs
+++ src/Contracts/Common/Messages/Message.cs
@@ -223,23 +223,3 @@ public class Message<T>(string? subjectName = null)
             },
         };
 }
-
-public enum MessageType
-{
-    MaximumLength = 1,
-    MinumumLength = 2,
-    ValidFormat = 3,
-    Found = 4,
-    Existence = 5,
-    Correct = 6,
-    Active = 7,
-    OuttaOption = 8,
-    GreaterThan = 9,
-    GreaterThanEqual = 10,
-    LessThan = 11,
-    LessThanEqual = 12,
-    Empty = 13,
-    Null = 14,
-    Unique = 15,
-    Strong = 16,
-}
