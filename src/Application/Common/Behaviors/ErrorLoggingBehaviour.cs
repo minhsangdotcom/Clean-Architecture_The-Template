003487d8e417c531fcb855cf73bfca08c792267b
Tran Minh Sang <minhsang.1mil@gmail.com>
2024-09-13 21:14:28 +0700

    revert code ErrorLoggingBehaviour

--- src/Application/Common/Behaviors/ErrorLoggingBehaviour.cs
+++ src/Application/Common/Behaviors/ErrorLoggingBehaviour.cs
@@ -5,29 +5,23 @@ namespace Application.Common.Behaviors;
 
 public sealed class ErrorLoggingBehaviour<TMessage, TResponse>(
     ILogger<ErrorLoggingBehaviour<TMessage, TResponse>> logger
-) : IPipelineBehavior<TMessage, TResponse>
-    where TMessage : IMessage
+) : MessageExceptionHandler<TMessage, TResponse>
+    where TMessage : notnull, IMessage
 {
-    public async ValueTask<TResponse> Handle(
+    protected override ValueTask<ExceptionHandlingResult<TResponse>> Handle(
         TMessage message,
-        CancellationToken cancellationToken,
-        MessageHandlerDelegate<TMessage, TResponse> next
+        Exception exception,
+        CancellationToken cancellationToken
     )
     {
-        try
-        {
-            return await next(message, cancellationToken);
-        }
-        catch (Exception exception)
-        {
-            logger.LogError(
-                "\n\n Server {exception} error is with message '{Message}'\n {StackTrace}\n at {DatetimeUTC} \n",
-                exception.GetType().Name,
-                exception.Message,
-                exception.StackTrace?.TrimStart(),
-                DateTimeOffset.UtcNow
-            );
-            throw;
-        }
+        logger.LogError(
+            "\n\n Server {exception} error is with message '{Message}'\n {StackTrace}\n at {DatetimeUTC} \n",
+            exception.GetType().Name,
+            exception.Message,
+            exception.StackTrace?.TrimStart(),
+            DateTimeOffset.UtcNow
+        );
+
+        return NotHandled;
     }
 }
