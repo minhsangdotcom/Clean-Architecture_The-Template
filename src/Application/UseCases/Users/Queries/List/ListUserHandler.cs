ad0c4d4af868b512faf05cb3a83a384f06007b59
Tran Minh Sang <minhsang.1mil@gmail.com>
2024-09-27 22:31:34 +0700

    refactor code

--- src/Application/UseCases/Users/Queries/List/ListUserHandler.cs
+++ src/Application/UseCases/Users/Queries/List/ListUserHandler.cs
@@ -1,16 +1,22 @@
 using Application.Common.Interfaces.Repositories;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Contracts.Dtos.Responses;
+using Contracts.Extensions.QueryExtensions;
 using Domain.Aggregates.Users;
 using Domain.Aggregates.Users.Specifications;
 using Mediator;
 
 namespace Application.UseCases.Users.Queries.List;
 
-public class ListUserHandler(IUnitOfWork unitOfWork) : IRequestHandler<ListUserQuery, PaginationResponse<ListUserResponse>>
+public class ListUserHandler(IUnitOfWork unitOfWork)
+    : IRequestHandler<ListUserQuery, PaginationResponse<ListUserResponse>>
 {
-    public async ValueTask<PaginationResponse<ListUserResponse>> Handle(ListUserQuery query, CancellationToken cancellationToken)
-    {
-        return await unitOfWork.Repository<User>()
+    public async ValueTask<PaginationResponse<ListUserResponse>> Handle(
+        ListUserQuery query,
+        CancellationToken cancellationToken
+    ) =>
+        await unitOfWork
+            .Repository<User>()
             .PaginatedListSpecificationAsync<ListUserResponse>(new ListUserSpecification(), query);
-    }
 }
