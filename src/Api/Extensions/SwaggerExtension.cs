014957f86b59c12adec394df7bc754574a3cac19
Tran Minh Sang <minhsang.1mil@gmail.com>
2024-10-02 01:40:19 +0700

    add open api document

--- src/Api/Extensions/SwaggerExtension.cs
+++ src/Api/Extensions/SwaggerExtension.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.OpenApi.Models;
 
@@ -8,32 +9,41 @@ public static class SwaggerExtension
     public static IServiceCollection AddSwagger(this IServiceCollection services)
     {
         return services.AddSwaggerGen(option =>
-         {
-             option.EnableAnnotations();
-             option.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme
-             {
-                 In = ParameterLocation.Header,
-                 Description = "Please enter token",
-                 Name = "Authorization",
-                 Type = SecuritySchemeType.Http,
-                 BearerFormat = "JWT",
-                 Scheme = "bearer"
-             });
-
-             option.AddSecurityRequirement(new OpenApiSecurityRequirement
-             {
+        {
+            option.EnableAnnotations();
+            option.AddSecurityDefinition(
+                JwtBearerDefaults.AuthenticationScheme,
+                new OpenApiSecurityScheme
                 {
-                    new OpenApiSecurityScheme
-                    {
-                        Reference = new OpenApiReference
-                        {
-                            Type=ReferenceType.SecurityScheme,
-                            Id="Bearer"
-                        }
-                    },
-                    Array.Empty<string>()
+                    In = ParameterLocation.Header,
+                    Description = "Please enter token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "bearer",
                 }
-             });
-         });
+            );
+
+            option.AddSecurityRequirement(
+                new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer",
+                            },
+                        },
+                        Array.Empty<string>()
+                    },
+                }
+            );
+
+            var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+            option.IncludeXmlComments(xmlPath);
+        });
     }
 }
