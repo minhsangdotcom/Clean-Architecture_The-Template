01d316d9323e58160f124e40ce5ab5089c154552
Tran Minh Sang <minhsang.1mil@gmail.com>
2024-10-10 14:54:25 +0700

    bind dynamic filter

--- src/Contracts/Binds/FilterModelBinder.cs
+++ src/Contracts/Binds/FilterModelBinder.cs
@@ -1,4 +1,6 @@
 using Contracts.Dtos.Requests;
+using Contracts.Extensions;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace Contracts.Binds;
@@ -7,16 +9,29 @@ public class FilterModelBinder : IModelBinder
 {
     public Task BindModelAsync(ModelBindingContext bindingContext)
     {
-        var query = bindingContext
-            .HttpContext.Request.QueryString.Value![1..]
-            .Split("&")
-            .Where(x =>
-                x.StartsWith(nameof(QueryParamRequest.Filter), StringComparison.OrdinalIgnoreCase)
-            )
-            .ToArray();
+        var queryString = GetQueryParams(bindingContext.HttpContext);
 
-        bindingContext.Result = ModelBindingResult.Success("");
+        bindingContext.Result = ModelBindingResult.Success(StringExtension.Parse(queryString));
 
         return Task.CompletedTask;
+    }
+
+    private static string[] GetQueryParams(HttpContext httpContext)
+    {
+        var queryStringValue = httpContext?.Request.QueryString.Value;
+
+        if (string.IsNullOrEmpty(queryStringValue))
+            return [];
+
+        var queryParams = queryStringValue[1..].Split("&");
+
+        return queryParams
+            .Where(param =>
+                param.StartsWith(
+                    nameof(QueryParamRequest.Filter),
+                    StringComparison.OrdinalIgnoreCase
+                )
+            )
+            .ToArray();
     }
 }
---
+++ src/Contracts/Extensions/StringExtension.LHSParser.cs
@@ -0,0 +1,158 @@
+using System.Dynamic;
+using Contracts.Dtos.Requests;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Primitives;
+
+namespace Contracts.Extensions;
+
+public partial class StringExtension
+{
+    public static object? Parse(string[] queries)
+    {
+        if (queries == null || queries.Length == 0)
+        {
+            return null;
+        }
+
+        dynamic filters = new ExpandoObject();
+        foreach (var query in queries)
+        {
+            Dictionary<string, StringValues> queryStrings = QueryHelpers.ParseQuery(query);
+            KeyValuePair<string, StringValues> queryString = queryStrings.ElementAt(0);
+
+            string[] keyList = queryString
+                .Key.Trim()
+                .Replace(
+                    nameof(QueryParamRequest.Filter),
+                    string.Empty,
+                    StringComparison.OrdinalIgnoreCase
+                )
+                .Split("]", StringSplitOptions.RemoveEmptyEntries);
+            List<string> cleanKey = keyList.Select(x => x.Replace("[", string.Empty)).ToList();
+            string value = queryString.Value.ToString();
+
+            dynamic leaf = ParseKey(cleanKey, value);
+            filters = Merge(filters, leaf);
+        }
+
+        return filters;
+    }
+
+    static dynamic ParseKey(List<string> keys, string value)
+    {
+        dynamic leaf = value;
+        for (int i = keys.Count - 1; i >= 0; i--)
+        {
+            dynamic obj = new ExpandoObject();
+            string key = keys[i];
+            if (IsDigit(key))
+            {
+                int index = int.Parse(key);
+                object[] element = new object[index + 1];
+                element[index] = leaf;
+                obj = element;
+            }
+            else
+            {
+                ((IDictionary<string, object>)obj)[key] = leaf;
+            }
+            leaf = obj;
+        }
+        return leaf;
+    }
+
+    static dynamic Merge(dynamic root, dynamic leaf)
+    {
+        var rootObject = (IDictionary<string, object>)root;
+        var leafObject = (IDictionary<string, object>)leaf;
+        var prop = leafObject.ElementAt(0);
+
+        if (!rootObject.Any() || !rootObject.ContainsKey(prop.Key))
+        {
+            rootObject.Add(new(prop.Key, prop.Value));
+            return rootObject;
+        }
+
+        foreach (var property in rootObject)
+        {
+            if (leafObject.TryGetValue(property.Key, out object? value))
+            {
+                Type type = property.Value.GetType();
+                Type leafValueType = value.GetType();
+
+                if (type != leafValueType)
+                {
+                    continue;
+                }
+
+                if (typeof(IEnumerable<object>).IsAssignableFrom(type))
+                {
+                    object[] values = ((IEnumerable<object>)value).ToArray();
+                    object[] rootValues = ((IEnumerable<object>)property.Value).ToArray();
+                    object[] results = new object[values.Count(x => x != null) + rootValues.Length];
+                    ConcatArray(rootValues, values, ref results);
+                    rootObject[property.Key] = results.Where(x => x != null).ToArray();
+                }
+                else
+                {
+                    var valueRoot = (IDictionary<string, object>)property.Value;
+                    var Valueleaf = (IDictionary<string, object>)value;
+                    rootObject[property.Key] = Merge(valueRoot, Valueleaf);
+                }
+            }
+        }
+
+        return rootObject;
+    }
+
+    static void ConcatArray(object[] root, object[] leaf, ref object[] results)
+    {
+        for (int i = 0; i < leaf.Length; i++)
+        {
+            object leafValue = leaf[i];
+
+            if (leafValue == null)
+            {
+                continue;
+            }
+            results[i] = leafValue;
+        }
+
+        for (int i = 0; i < root.Length; i++)
+        {
+            object rootValue = root[i];
+
+            if (rootValue == null)
+            {
+                continue;
+            }
+
+            if (results[i] != null)
+            {
+                results[i] = Merge(results[i], rootValue);
+            }
+            else
+            {
+                results[i] = rootValue;
+            }
+        }
+    }
+
+    static bool IsDigit(string str)
+    {
+        if (string.IsNullOrEmpty(str))
+        {
+            return false;
+        }
+
+        foreach (char c in str)
+        {
+            if (!char.IsDigit(c))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
