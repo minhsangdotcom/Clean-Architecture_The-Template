a850510730b0f5ba3144dedd267b46d0480644a8
Minh Sáng <minhsang.1mil@gmail.com>
2025-01-17 13:28:06 +0700

    add comment for settings

--- src/Infrastructure/Services/Elastics/ElasticsearchSettings.cs
+++ src/Infrastructure/Services/Elastics/ElasticsearchSettings.cs
@@ -8,6 +8,9 @@ public class ElasticsearchSettings
 
     public string? Password { get; set; }
 
+    /// <summary>
+    /// Dicide to use elasticsearch or not
+    /// </summary>
     public bool IsEnbaled { get; set; }
 
     public string? DefaultIndex { get; set; } = "DefaultIndex";
