bd286d19ecf5a6b37a07b64a50ce41d2c77a2202
Minh Sáng <minhsang.1mil@gmail.com>
2025-04-25 23:02:29 +0700

    Remove commented-out mapping configurations for roles and users in RoleMapping and UserMapping classes.

--- src/Application/Features/Common/Mapping/Users/UserMapping.cs
+++ src/Application/Features/Common/Mapping/Users/UserMapping.cs
@@ -3,58 +3,6 @@ using Domain.Aggregates.Users;
 
 namespace Application.Features.Common.Mapping.Users;
 
-// public class UserMapping : Profile
-// {
-//     public UserMapping()
-//     {
-//         CreateMap<User, UserProjection>().IncludeMembers(x => x.Address);
-
-//         CreateMap<User, UserDetailProjection>()
-//             .IncludeMembers(x => x.Address)
-//             .ForMember(
-//                 dest => dest.Roles,
-//                 opt => opt.MapFrom(src => src.UserRoles!.Select(x => x.Role))
-//             );
-//         CreateMap<Address, UserProjection>();
-//         CreateMap<Address, UserDetailProjection>();
-
-//         CreateMap<Role, RoleDetailProjection>();
-//         CreateMap<RoleClaim, RoleClaimDetailProjection>();
-//         CreateMap<UserClaim, UserClaimDetailProjection>();
-
-//         CreateMap<UserClaimModel, UserClaim>()
-//             .AfterMap(
-//                 (src, dest, context) =>
-//                 {
-//                     if (
-//                         Enum.TryParse(
-//                             context.Items[nameof(UserClaim.Type)]?.ToString(),
-//                             out KindaUserClaimType type
-//                         )
-//                     )
-//                     {
-//                         dest.Type = type;
-//                     }
-
-//                     if (
-//                         Ulid.TryParse(
-//                             context.Items[nameof(UserClaim.UserId)]?.ToString(),
-//                             out Ulid id
-//                         )
-//                     )
-//                     {
-//                         dest.UserId = id;
-//                     }
-
-//                     if (src.Id == null || src.Id == Ulid.Empty)
-//                     {
-//                         dest.Id = Ulid.NewUlid();
-//                     }
-//                 }
-//             );
-//     }
-// }
-
 public static class UserMapping
 {
     public static UserClaimDetailProjection ToRoleClaimDetailProjection(this UserClaim userClaim)
