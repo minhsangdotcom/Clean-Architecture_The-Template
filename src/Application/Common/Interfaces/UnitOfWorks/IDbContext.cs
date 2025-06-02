d40f4da122ce1b8a5aa0e887e3721ece1cb3dd81
Minh Sáng <minhsang.1mil@gmail.com>
2025-06-02 12:58:40 +0700

    revert: ef core bulk extension

--- src/Application/Common/Interfaces/UnitOfWorks/IDbContext.cs
+++ src/Application/Common/Interfaces/UnitOfWorks/IDbContext.cs
@@ -1,4 +1,3 @@
-using EFCore.BulkExtensions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Infrastructure;
@@ -13,18 +12,6 @@ public interface IDbContext : IDisposable
         where TEntity : class;
 
     public DatabaseFacade DatabaseFacade { get; }
-
-    public Task DeleteRangeAsync<T>(IEnumerable<T> entities, Action<BulkConfig>? bulkConfig = null,
-        CancellationToken cancellation = default) where T : class;
-
-    public Task InsertRangeAsync<T>(IEnumerable<T> entities, Action<BulkConfig>? bulkConfig = null,
-        CancellationToken cancellation = default) where T : class;
-
-    public Task UpdateRangeAsync<T>(IEnumerable<T> entities, Action<BulkConfig>? bulkConfig = null,
-        CancellationToken cancellation = default) where T : class;
-
-    public Task SynchronizeAsync<T>(IEnumerable<T> entities, Action<BulkConfig>? bulkConfig = null,
-        CancellationToken cancellation = default) where T : class;
-
+    
     Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
 }
--- src/Infrastructure/Data/TheDbContext.cs
+++ src/Infrastructure/Data/TheDbContext.cs
@@ -1,7 +1,6 @@
 using System.Reflection;
 using Application.Common.Interfaces.UnitOfWorks;
 using Domain.Common;
-using EFCore.BulkExtensions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 
@@ -10,39 +9,6 @@ namespace Infrastructure.Data;
 public class TheDbContext(DbContextOptions<TheDbContext> options) : DbContext(options), IDbContext
 {
     public DatabaseFacade DatabaseFacade => Database;
-
-    public async Task DeleteRangeAsync<T>(IEnumerable<T> entities, Action<BulkConfig>? bulkConfig = null,
-        CancellationToken cancellation = default) where T : class
-    {
-        BulkConfig defaultConfig = GetDefaultConfig();
-        bulkConfig?.Invoke(defaultConfig);
-        await this.BulkDeleteAsync(entities, defaultConfig, cancellationToken: cancellation);
-    }
-
-    public async Task InsertRangeAsync<T>(IEnumerable<T> entities,  Action<BulkConfig>? bulkConfig = null, CancellationToken cancellation = default) where T : class
-    {
-        BulkConfig defaultConfig = GetDefaultConfig();
-        bulkConfig?.Invoke(defaultConfig);
-        await this.BulkInsertAsync(entities, defaultConfig, cancellationToken: cancellation);
-    }
-
-    public async Task UpdateRangeAsync<T>(IEnumerable<T> entities, Action<BulkConfig>? bulkConfig = null,
-        CancellationToken cancellation = default) where T : class
-    {
-        BulkConfig defaultConfig = GetDefaultConfig();
-        bulkConfig?.Invoke(defaultConfig);
-        await this.BulkUpdateAsync(entities, defaultConfig, cancellationToken: cancellation);
-    }
-
-    public async Task SynchronizeAsync<T>(IEnumerable<T> entities, Action<BulkConfig>? bulkConfig = null, CancellationToken cancellation = default) where T : class
-    {
-        BulkConfig defaultConfig = GetDefaultConfig();
-        bulkConfig?.Invoke(defaultConfig);
-        await this.BulkInsertOrUpdateOrDeleteAsync(entities, defaultConfig, cancellationToken: cancellation);
-    }
-
-    private static BulkConfig GetDefaultConfig() => new BulkConfig()
-        { BatchSize = 1000, PreserveInsertOrder = false, BulkCopyTimeout = 60, SqlBulkCopyOptions = SqlBulkCopyOptions.KeepIdentity};
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
