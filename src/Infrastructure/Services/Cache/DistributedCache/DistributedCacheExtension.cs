74c342d928d59af2f2cfc19f53cabc93f95c5469
Minh Sáng <minhsang.1mil@gmail.com>
2025-08-17 14:21:20 +0700

    chore: update cache and razor render html

--- src/Infrastructure/Services/Cache/DistributedCache/DistributedCacheExtension.cs
+++ src/Infrastructure/Services/Cache/DistributedCache/DistributedCacheExtension.cs
@@ -1,6 +1,8 @@
 using Application.Common.Interfaces.Services.Cache;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using StackExchange.Redis;
 
 namespace Infrastructure.Services.Cache.DistributedCache;
 
@@ -21,6 +23,18 @@ public static class DistributedCacheExtension
                 .Configure<RedisDatabaseSettings>(options =>
                     configuration.GetSection(nameof(RedisDatabaseSettings)).Bind(options)
                 )
+                .AddSingleton(sp =>
+                {
+                    var settings = sp.GetRequiredService<IOptions<RedisDatabaseSettings>>().Value;
+                    ConfigurationOptions options =
+                        new()
+                        {
+                            EndPoints = { { settings.Host!, settings.Port!.Value } },
+                            Password = settings.Password,
+                        };
+                    ConnectionMultiplexer multiplexer = ConnectionMultiplexer.Connect(options);
+                    return multiplexer.GetDatabase();
+                })
                 .AddSingleton<IDistributedCacheService, RedisCacheService>();
         }
 
